1d7a21bc80285bf00b0a775540cd868d95215a9d
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-11 17:10:48 +0000

    Frontend has been started

---
+++ 1-CRUDOperationsForBooks/frontend/src/components/BookForm.jsx
@@ -0,0 +1,14 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const BookForm = () => {};
+
+export default BookForm;
